41b15061c7114c40fa1e73aeaa9e52b478c53c04
Guru18650 <73262552+Guru18650@users.noreply.github.com>
2021-12-21 13:25:01 +0100

    gitignore i naprawiona literówka

--- vulcanbot.py
+++ vulcanbot.py
@@ -157,8 +157,7 @@ async def send():
             await client.close()
         else:
             embedVar = discord.Embed(title="Lekcja rozpoczęta", description=str(date.today()) + ", " + str(days[date.today().weekday()]), color=0x00ff00)
-            for lesson in lekcje:
-               embedVar.add_field(name=str(lesson.pozycja) + ". " + lesson.przedmiot, value=lesson.imienazwisko, inline=False)
+            embedVar.add_field(name=str(lekcje[lnumber].pozycja) + ". " + lekcje[lnumber].przedmiot, value=lekcje[lnumber].imienazwisko, inline=False)
             channel = dcclient.get_channel(777899451914125342)
             await channel.send(embed=embedVar)
             await client.close()
